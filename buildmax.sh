eb267fbb48bc92a9186bf30e5cd3715cbfd0d4d7
Bipolairbeer <47387168+Bipolairbeer@users.noreply.github.com>
2020-04-05 04:44:07 -0400

    Installing for you

---
+++ buildmax.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+sudo dnf -y install make git pacman gnupg createrepo rpm-build make wget rpmdevtools python3-sh dialog rpm-sign dpkg-dev debootstrap PyYAML devscripts perl-Digest-MD5 perl-Digest-SHA
+sudo dnf -y update && sudo dnf -y upgrade
+
+sudo git clone https://github.com/QubesOS/qubes-secpack.git /home/user/qubes-secpack
+sudo chmod -R 777 /home/user/qubes-secpack
+
+gpg --import qubes-secpack/keys/*/*
+gpg --command-fd 0 --edit-key 36879494 <<EOF
+fpr
+trust
+5
+y
+quit
+EOF
+
+cd /home/user/qubes-secpack/canaries/
+gpg --verify canary-001-2015.txt.sig.joanna canary-001-2015.txt
+gpg --verify canary-001-2015.txt.sig.marmarek canary-001-2015.txt
+
+cd
+sudo git clone https://github.com/QubesOS/qubes-builder.git /home/user/qubes-builder
+sudo chmod -R 777 /home/user/qubes-builder
+
+cd /home/user/qubes-builder/
+git tag -v `git describe`
+
+cd
+sudo cp /home/user/Qubes-MAX/builder.conf /home/user/qubes-builder/builder.conf
+
+cd /home/user/qubes-builder/
+make install-deps
+make get-sources
+make qubes
+make iso
